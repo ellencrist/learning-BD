9056fb2bbdbbd645ced1a69ea6551c28eb17abe5
Ellen Cristine <86074197+ellencrist@users.noreply.github.com>
2023-04-19 23:59:25 -0300

    Add files via upload

---
+++ GeneXus 17 Course/04-07 - Exercícios.sql
@@ -0,0 +1,59 @@
+-- ###################################################################### --
+-- ========= SE PREPARANDO PARA UMA PROVA P/ ANALISTA DE DADOS SQL =========
+-- ###################################################################### --
+
+
+-- Exercício 1. Você é Analista de Customer Experience (Experiência do Cliente) e precisa criar um relatório com os 100 primeiros clientes da história da empresa. Esses clientes receberão uma placa em reconhecimento à confiança dada.
+
+-- Você precisa levantar essa lista de clientes dentro do banco de dados, em 10 minutos, pois o seu gestor solicitou essa informação para apresentar em uma reunião.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- Exercício 2. Você é Analista de Produtos e precisa levantar algumas informações sobre os produtos, como:
+-- Quantidade de Produtos
+-- Soma total de peso dos produtos
+-- Preço Médio dos Produtos
+-- Maior Preço
+-- Menor Preço
+
+-- Utilize seus conhecimentos em SQL para fazer essas análises dentro do Banco de Dados.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- 3. Você agora é um Analista de RH da empresa e precisa saber a quantidade total de funcionários de cada departamento pois o Financeiro depende desse entendimento para dimensionar gastos para cada um dos departamentos, como bonificações, computadores, equipamentos de forma geral. Utilize seus conhecimentos para dar suporte à área financeira.
+
+
+
+
